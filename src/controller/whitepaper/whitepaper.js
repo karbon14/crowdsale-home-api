9c417792511bf00462d605a4213a8e3e8a1a2beb
zalazar natanael <zalazar.natanael@gmail.com>
2018-07-02 21:22:38 -0300

    Feature: Modify env.example, add query lang in whitepaper route and update test

--- src/controller/whitepaper/whitepaper.js
+++ src/controller/whitepaper/whitepaper.js
@@ -1,12 +1,22 @@
 import { resolve } from 'path'
 import { createReadStream } from 'fs'
+import Joi from 'joi'
+
+export const whitepaperSchema = () => ({
+  lang: Joi.string()
+    .required()
+    .min(2)
+    .max(2)
+})
 
 export const whitepaper = (configuration) => (ctx) => {
-  const { WHITEPAPER_FILE } = configuration
-  const fileName = resolve(__dirname, `../../public/${WHITEPAPER_FILE}`)
+  const lang = ctx.query && ctx.query.lang ? ctx.query.lang : 'EN'
+  const whitepaperName = configuration[`WHITEPAPER_FILE_${lang.toUpperCase()}`]
+  
+  const fileName = resolve(__dirname, `../../public/${whitepaperName}`)
   ctx.body = createReadStream(fileName)
 
-  ctx.set('Content-disposition', `attachment; filename=${WHITEPAPER_FILE}`)
+  ctx.set('Content-disposition', `attachment; filename=${whitepaperName}`)
   ctx.set('Content-Type', 'application/force-download')
   ctx.status = 200
 }
