c9a257b4a08ede8fbb39ac320b73306097a6f4b9
zalazar natanael <zalazar.natanael@gmail.com>
2018-07-06 17:57:44 -0300

    fix

--- src/controller/whitepaper/whitepaper.js
+++ src/controller/whitepaper/whitepaper.js
@@ -8,7 +8,6 @@ export const whitepaperSchema = () => ({
     .max(2),
 })
 
-
 export const whitepaper = (configuration) => (ctx) => {
   const lang = ctx.query && ctx.query.lang ? ctx.query.lang : 'EN'
   const whitepaperName = configuration[`WHITEPAPER_FILE_${lang.toUpperCase()}`]
