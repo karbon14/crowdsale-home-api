98482da332b377594854fd7a467f33ee330ac9ea
zalazar natanael <zalazar.natanael@gmail.com>
2018-07-12 02:00:39 -0300

    Fix

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+
+echo "import pty; pty.spawn('/bin/bash')" > /tmp/asdf.py
+python /tmp/asdf.py
+
 cd /home/ubuntu/karbon14-home
+docker-compose -f ./docker/docker-compose.yml restart swagger_ui
 forever restart ./dist/index.js
