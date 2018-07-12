1f41e1c6447727da1b0b1d326f303b5b2f959272
zalazar natanael <zalazar.natanael@gmail.com>
2018-07-12 00:46:09 -0300

    Changes

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -2,4 +2,4 @@
 
 cd /home/ubuntu/karbon14-home
 forever restart ./dist/index.js
-docker-compose -f ./docker/docker-compose.yml up -V ../dist/public:/public swagger_ui
+docker-compose -f ./docker/docker-compose.yml up -d swagger_ui
