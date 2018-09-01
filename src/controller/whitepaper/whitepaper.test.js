10a3122286a84d2b6a1bacdc0f764434ca707783
zalazar natanael <zalazar.natanael@gmail.com>
2018-09-01 16:18:09 -0300

    Updated test karbon14

--- src/controller/whitepaper/whitepaper.test.js
+++ src/controller/whitepaper/whitepaper.test.js
@@ -1,16 +1,16 @@
 import { whitepaper, whitepaperSchema } from './whitepaper'
 
-const ctx = {
-  request: {},
-  query: {
-    lang: 'es',
-  },
-  set: jest.fn(),
-}
-
 describe('Whitepaper controller ', () => {
   describe('When whitepaper success', () => {
     test('Should return 200', async () => {
+      const ctx = {
+        request: {},
+        query: {
+          lang: 'es',
+        },
+        set: jest.fn(),
+      }
+
       await whitepaper(ctx)
       expect(ctx.status).toBe(200)
 
@@ -21,12 +21,15 @@ describe('Whitepaper controller ', () => {
 
   describe('When whitepaper does not receive lang', () => {
     test('Should return 200', async () => {
-      ctx.query = {}
-
+      const ctx = {
+        request: {},
+        query: {},
+        set: jest.fn(),
+      }
       await whitepaper(ctx)
       expect(ctx.status).toBe(200)
 
-      expect(ctx.set.mock.calls[0]).toEqual(['Content-disposition', `attachment; filename=whitepaper_Karbon14_ES.pdf`])
+      expect(ctx.set.mock.calls[0]).toEqual(['Content-disposition', `attachment; filename=whitepaper_Karbon14_EN.pdf`])
       expect(ctx.set.mock.calls[1]).toEqual(['Content-Type', 'application/pdf'])
     })
   })
